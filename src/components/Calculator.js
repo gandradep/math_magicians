88f2cee8a521d3a4894da80beb3969b21a36a755
gandradep <eventsonido@gmail.com>
2022-10-27 12:31:12 +0100

    working calculator. Lifting up state

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
 import React from 'react';
 import SingleDiv from './SingleDiv';
 import CalcDisplay from './CalcDisplay';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
   constructor(props) {
@@ -25,19 +27,46 @@ class Calculator extends React.PureComponent {
       { id: '0', value: '0', classy: 'gray zeroButton' },
       { id: 'decimal', value: '.', classy: 'gray' },
       { id: 'equal', value: '=', classy: 'gray' },
-
     ];
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+
+  }
+
+  handleClick(value) {
+    const afterCalc = calculate(this.state, value);
+    console.log(afterCalc);
+    this.setState(afterCalc);
+
   }
 
   render() {
+    let displayValue = '0';
+    const { total, next } = this.state;
+
+    if (next){
+      displayValue = next;
+    } else {
+      if(total === null){
+        displayValue = '0';
+      } else {
+        displayValue = total;
+      }
+    }
     return (
       <div id="calculator">
-        <CalcDisplay key="display" name="0" classy="gray displayCalculator" />
+        <CalcDisplay key="display" name={displayValue} classy="gray displayCalculator" />
         {this.buttonValues.map((item) => (
           <SingleDiv
             key={item.id}
             name={item.value}
             classy={item.classy}
+            handleClick={this.handleClick}
           />
         ))}
 
--- src/components/SingleDiv.js
+++ src/components/SingleDiv.js
@@ -1,10 +1,24 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 class SingleDiv extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.handleClick(e.target.value);
+  }
+
   render() {
     const { classy, name } = this.props;
-    return <button type="button" className={classy}>{name}</button>;
+    return (
+      <button value={name} type="button" className={classy} onClick={this.handleClick}>
+        {name}
+      </button>
+    );
   }
 }
 SingleDiv.propTypes = {
