a942b720493624176c168d541d14266feb9c3ddd
gandradep <eventsonido@gmail.com>
2022-11-01 11:25:07 +0100

    Style to navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,27 @@ class Home extends React.PureComponent {
   render() {
     return (
       <div>
-        <h1> Home </h1>
+        <h2> Welcome to our page </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Aenean placerat vel felis vestibulum bibendum. Duis laoreet dui in
+          ex consectetur, vel sollicitudin velit cursus. Sed accumsan elit eu
+          libero pellentesque pretium. Integer id lorem non dui commodo
+          condimentum eget in lorem. Nulla eget elementum dolor. Donec
+          tincidunt nibh lorem, id luctus elit volutpat et. Praesent non
+          tortor posuere, accumsan lacus sit amet, vehicula lacus. Sed urna
+          mauris, pretium quis lectus vel, pretium lobortis velit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Aenean placerat vel felis vestibulum bibendum. Duis laoreet dui in
+          ex consectetur, vel sollicitudin velit cursus. Sed accumsan elit eu
+          libero pellentesque pretium. Integer id lorem non dui commodo
+          condimentum eget in lorem. Nulla eget elementum dolor. Donec
+          tincidunt nibh lorem, id luctus elit volutpat et. Praesent non
+          tortor posuere, accumsan lacus sit amet, vehicula lacus. Sed urna
+          mauris, pretium quis lectus vel, pretium lobortis velit.
+        </p>
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ class Navbar extends React.PureComponent {
   render() {
     return (
       <nav className="dFlex nav">
-        <h1> Math Magicians </h1>
-        <ul className="dFlex menu">
+        <h1 className="fsizeBig fw400"> Math Magicians </h1>
+        <ul className="dFlex menu fs175">
           <li>Home</li>
           <li>|</li>
           <li>Calculator</li>
