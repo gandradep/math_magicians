3a98529f4edab12ca59c4ea7e188c97b3199b302
gandradep <eventsonido@gmail.com>
2022-11-02 15:45:41 +0100

    Test for +/- symbol

--- src/testFiles/calculate.test.js
+++ src/testFiles/calculate.test.js
@@ -2,15 +2,29 @@ import calculate from '../logic/calculate';
 
 describe('calculate tests', () => {
   const objInput = {
-    total: 3,
-    next: 6,
-    operation: null,
+    total: '3',
+    next: '6',
+    operation: '+',
   };
   test('AC test', () => {
-    expect(calculate(objInput, 'AC')).toStrictEqual({      
+    expect(calculate(objInput, 'AC')).toStrictEqual({
       next: null,
       operation: null,
       total: null,
+    });
+  });
+  test('decimal dot test', () => {
+    expect(calculate(objInput, '.')).toStrictEqual({
+      total: '3',
+      next: '6.',
+      operation: '+',
+    });
+  });
+  test('+/- symbol', () => {
+    expect(calculate(objInput, '+/-')).toStrictEqual({
+      total: '3',
+      next: '-6',
+      operation: '+',
     });
   });
 });
