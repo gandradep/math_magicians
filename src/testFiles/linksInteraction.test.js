533af1b31f83141a0979ef1411aef017e240c2a6
gandradep <eventsonido@gmail.com>
2022-11-03 07:21:45 +0100

    user interaction tests

---
+++ src/testFiles/linksInteraction.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import App from '../App';
+
+describe('links interaction testing', () => {
+  test('testing quote link', () => {
+    render(<App />);
+    const inputElement = screen.getByText(/quote/i)
+    fireEvent.click(inputElement);
+  });
+  test('testing calculator link', () => {
+    render(<App />);
+    const inputElement = screen.getByText(/calculator/i)
+    fireEvent.click(inputElement);
+  });
+});
